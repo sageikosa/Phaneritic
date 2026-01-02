6a1ad7cdc06a49677f80c9f87b28ccb3aa2b8929
James Ousey <jdousey@hotmail.com>
2026-01-02 16:48:22 -0500

    even more cleanup

--- ps/dotnet ef migrations remove.ps1
+++ ps/dotnet ef migrations remove.ps1
@@ -11,25 +11,7 @@ $proj = "Phaneritic.Implementations"
 $proj
 dotnet ef migrations remove -p $proj --context TableFreshnessContext -- krnl $ConnStr
 
-$proj = "GyroLedger.Intralogistics.Implementations"
-$proj
-write-output "----- BaseItemMasterContext -----"
-dotnet ef migrations remove -p $proj --context BaseItemMasterContext -- itm $ConnStr
-write-output "----- OperationalContext -----"
-dotnet ef migrations remove -p $proj --context OperationalContext -- op $ConnStr
-write-output "----- BaseInventoryContext -----"
-dotnet ef migrations remove -p $proj --context BaseInventoryContext -- inv $ConnStr
-write-output "----- BaseDemandContext -----"
-dotnet ef migrations remove -p $proj --context BaseDemandContext -- dmnd $ConnStr
-write-output "----- FulfillSourceContext -----"
-dotnet ef migrations remove -p $proj --context FulfillSourceContext -- ffsrc $ConnStr
-write-output "----- FulfillDestinationContext -----"
-dotnet ef migrations remove -p $proj --context FulfillDestinationContext -- ffdest $ConnStr
-write-output "----- FulfillFlowContext -----"
-dotnet ef migrations remove -p $proj --context FulfillFlowContext -- ffflow $ConnStr
-write-output "----- BaseMovementContext -----"
-dotnet ef migrations remove -p $proj --context BaseMovementContext -- mv $ConnStr
-write-output "----- LedgeringContext -----"
-dotnet ef migrations remove -p $proj --context LedgeringContext -- ldgr $ConnStr
-write-output "----- RateContext -----"
-dotnet ef migrations remove -p $proj --context RateContext -- rate $ConnStr
+# $proj = "Phaneritic.Operational.Implementations"
+# $proj
+# write-output "----- OperationalContext -----"
+# dotnet ef migrations remove -p $proj --context OperationalContext -- op $ConnStr
--- ps/dotnet ef migrations script.ps1
+++ ps/dotnet ef migrations script.ps1
@@ -11,25 +11,7 @@ $proj = "Phaneritic.Implementations"
 $proj
 dotnet ef migrations script 0 -p $proj -o "SQL\krnlinit.sql" --context TableFreshnessContext -- krnl $ConnStr
 
-$proj = "GyroLedger.Intralogistics.Implementations"
-$proj
-write-output "----- BaseItemMasterContext -----"
-dotnet ef migrations script 0 -p $proj -o "SQL\itminit.sql" --context BaseItemMasterContext -- itm $ConnStr
-write-output "----- OperationalContext -----"
-dotnet ef migrations script 0 -p $proj -o "SQL\opinit.sql" --context OperationalContext -- op $ConnStr
-write-output "----- BaseInventoryContext -----"
-dotnet ef migrations script 0 -p $proj -o "SQL\invinit.sql" --context BaseInventoryContext -- inv $ConnStr
-write-output "----- BaseDemandContext -----"
-dotnet ef migrations script 0 -p $proj -o "SQL\dmndinit.sql" --context BaseDemandContext -- dmnd $ConnStr
-write-output "----- FulfillSourceContext -----"
-dotnet ef migrations script 0 -p $proj -o "SQL\ffsrcinit.sql" --context FulfillSourceContext -- ffsrc $ConnStr
-write-output "----- FulfillDestinationContext -----"
-dotnet ef migrations script 0 -p $proj -o "SQL\ffdestinit.sql" --context FulfillDestinationContext -- ffdest $ConnStr
-write-output "----- FulfillFlowContext -----"
-dotnet ef migrations script 0 -p $proj -o "SQL\ffflowinit.sql" --context FulfillFlowContext -- ffflow $ConnStr
-write-output "----- BaseMovementContext -----"
-dotnet ef migrations script 0 -p $proj -o "SQL\mvinit.sql" --context BaseMovementContext -- mv $ConnStr
-write-output "----- LedgeringContext -----"
-dotnet ef migrations script 0 -p $proj -o "SQL\ldgrinit.sql" --context LedgeringContext -- ldgr $ConnStr
-write-output "----- RateContext -----"
-dotnet ef migrations script 0 -p $proj -o "SQL\rateinit.sql" --context RateContext -- rate $ConnStr
+# $proj = "Phaneritic.Operational.Implementations"
+# $proj
+# write-output "----- OperationalContext -----"
+# dotnet ef migrations script 0 -p $proj -o "SQL\opinit.sql" --context OperationalContext -- op $ConnStr
