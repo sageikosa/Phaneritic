222ee6778325725e8ef6aba8a8fe934f0c3230f8
James Ousey <jdousey@hotmail.com>
2026-02-27 13:08:27 -0500

    improved detail type and stage type...with migrations also

--- Phaneritic.Implementations/EF/Migrations/Kernel/20260227180636_Initialize.Designer.cs
+++ Phaneritic.Implementations/EF/Migrations/Kernel/20260227180636_Initialize.Designer.cs
@@ -12,7 +12,7 @@ using Phaneritic.Implementations.EF.TableCache;
 namespace Phaneritic.Implementations.EF.Migrations.Kernel
 {
     [DbContext(typeof(TableFreshnessContext))]
-    [Migration("20260226203058_Initialize")]
+    [Migration("20260227180636_Initialize")]
     partial class Initialize
     {
         /// <inheritdoc />
