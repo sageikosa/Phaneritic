8e988dbfdcd995474803ec722a030e58ee40b762
James Ousey <jdousey@hotmail.com>
2026-03-05 11:42:38 -0500

    removed stageStateKey and StageState

--- Phaneritic.Implementations/EF/Migrations/Kernel/20260305163503_Initialize.Designer.cs
+++ Phaneritic.Implementations/EF/Migrations/Kernel/20260305163503_Initialize.Designer.cs
@@ -12,7 +12,7 @@ using Phaneritic.Implementations.EF.TableCache;
 namespace Phaneritic.Implementations.EF.Migrations.Kernel
 {
     [DbContext(typeof(TableFreshnessContext))]
-    [Migration("20260304215513_Initialize")]
+    [Migration("20260305163503_Initialize")]
     partial class Initialize
     {
         /// <inheritdoc />
